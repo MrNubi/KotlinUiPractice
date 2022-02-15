c6763043e373426a706ea3301994da0ecf1913e7
MrNubi <99630193+MrNubi@users.noreply.github.com>
2022-02-15 14:47:54 +0900

    코틀린 속성값 방법 1.get xml에서 아이디를 바탕으로 불러서 사용 id값.속성이라고 찍으면 그 속성 조회 가능

--- app/src/main/java/com/beyond/kotlinuipractice/MainActivity.kt
+++ app/src/main/java/com/beyond/kotlinuipractice/MainActivity.kt
@@ -2,10 +2,23 @@ package com.beyond.kotlinuipractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Btn1.setOnClickListener {
+            val inputMessage = ET1.text.toString()
+
+            Toast.makeText(this, "$inputMessage", Toast.LENGTH_SHORT).show()
+
+
+
+
+        }
+
     }
 }
